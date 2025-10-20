2b64060c62fe904ba0d26b9930137b9755af5228
brakobby <brakobby454@gmail.com>
2025-10-20 18:05:45 +0000

    code committed

---
+++ Ecom/login/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def LoginView(request):
+    return HttpResponse("This application is under development")
