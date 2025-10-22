6ca28713339593c8926ca881bb59dbc34e698a10
brakobby <brakobby454@gmail.com>
2025-10-22 18:04:44 +0000

    removed login

--- Ecom/login/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def LoginView(request):
-    return HttpResponse("This application is under development")
